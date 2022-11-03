09e888f0b800df113abab537e1dbecde05787b9e
Varun Pradeep <varunpradeep30@gmail.com>
2022-11-03 20:39:20 +0530

    add create query generator

--- SQL_generator.py
+++ SQL_generator.py
@@ -1,9 +1,4 @@
-<<<<<<< Updated upstream
-
-# CREATE query 
-=======
 # CREATE query
->>>>>>> Stashed changes
 def create():
     table = input("Table name : ")
     cols = list(input("space separated column names :").split())
@@ -29,63 +24,10 @@ def create():
     else:
         result += ");"
     return result
-<<<<<<< Updated upstream
-    
-# INSERT query 
-def insert():
-    table = input("Table name : ")
-    cols = list(input("space separated column names :").split())
-    data = []
-    while True:
-        line = input()
-        if line:
-            data.append(line)
-        else:
-            break
-        
-    #to_date(current_date,'dd/mm/yyyy')
-    
-    result = "INSERT INTO {} (".format(table)
-    for i, col in enumerate(cols):
-        result += col
-        if i == len(cols)-1:
-            result += ") values ("
-        else:
-            result += "' " 
-    
-    
-    for values in data:
-        if "Manager" in values:
-            values = values.replace("Manager", "Manager NULL")
-        values = values.split()
-        
-        result = "INSERT INTO {} (".format(table)
-        for i, col in enumerate(cols):
-            result += col
-            if i == len(cols)-1:
-                result += ") values ("
-            else:
-                result += ", "
-        
-        for value in values:
-            print(value)
-    
-=======
->>>>>>> Stashed changes
 
 
 # DRIVER CODE
 command = input("command :")
 
 if command == 'create':
-<<<<<<< Updated upstream
     print(create())
-elif command == 'insert':
-    insert()
-
-
-
-
-=======
-    print(create())
->>>>>>> Stashed changes
