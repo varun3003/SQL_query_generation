1613847cd96b5161ab85ef8551cf54f3fd482634
Varun Pradeep <varunpradeep30@gmail.com>
2022-11-03 20:41:25 +0530

    add insert generator (incomplete)

--- SQL_generator.py
+++ SQL_generator.py
@@ -25,9 +25,49 @@ def create():
         result += ");"
     return result
 
+# INSERT query
+def insert():
+    table = input("Table name : ")
+    cols = list(input("space separated column names :").split())
+    data = []
+    while True:
+        line = input()
+        if line:
+            data.append(line)
+        else:
+            break
+        
+    #to_date(current_date,'dd/mm/yyyy')
+    
+    result = "INSERT INTO {} (".format(table)
+    for i, col in enumerate(cols):
+        result += col
+        if i == len(cols)-1:
+            result += ") values ("
+        else:
+            result += "' " 
+    
+    
+    for values in data:
+        if "Manager" in values:
+            values = values.replace("Manager", "Manager NULL")
+        values = values.split()
+        
+        result = "INSERT INTO {} (".format(table)
+        for i, col in enumerate(cols):
+            result += col
+            if i == len(cols)-1:
+                result += ") values ("
+            else:
+                result += ", "
+        
+        for value in values:
+          print(value)
 
 # DRIVER CODE
 command = input("command :")
 
 if command == 'create':
     print(create())
+elif command == 'insert':
+    insert()
